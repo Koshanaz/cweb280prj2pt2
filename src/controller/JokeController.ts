ed98953eb6ebc777db2b568ef47383332cca1af6
TrystanH <haus2811@saskpolytech.ca>
2022-11-09 02:40:42 -0600

    Got the delete, get, and put done!

--- src/controller/JokeController.ts
+++ src/controller/JokeController.ts
@@ -3,8 +3,22 @@ import {NextFunction, Request, Response} from "express";
 import {Joke} from "../entity/Joke";
 import {Controller} from '../decorator/Controller';
 import {Route} from '../decorator/Route';
+import * as createError from "http-errors";
 import { NotFoundException } from '@nestjs/common';
-import {validate, ValidatorOptions} from 'class-validator';
+import {IsNotEmpty, Max, Min, validate, ValidatorOptions} from 'class-validator';
+
+class UserRating {
+
+    @IsNotEmpty({message: 'id is required'})
+    @Min(1, {message: 'Minimum value is 1'})
+    id: number;
+
+    @IsNotEmpty({message: 'UserRating is required'})
+    @Min(1.0, {message: 'Lowest rating possible is 1.0'})
+    @Max(5.0, {message: 'Highest rating possible is 5.0'})
+    userRating: number;
+}
+
 
 @Controller('/jokes')
 export class JokeController {
@@ -14,27 +28,47 @@ export class JokeController {
 
     @Route('get')
     async all(request: Request, response: Response, next: NextFunction) {
-        return this.jokeRepository.find();
+        return this.jokeRepository.find({
+            order: {
+                id: "ASC",
+            },
+        });
     }
 
-    @Route('get', '/:id')
-    async one(request: Request, response: Response, next: NextFunction) {
-        let jokeToFind = await this.jokeRepository.findOne(request.params.id);
-        if(jokeToFind) {
-            return jokeToFind;
-        } else next();
+    @Route('get', '/:order')
+    async allSorts(request: Request, response: Response, next: NextFunction) {
+        let sortBy = request.params.order;
+        // Check if sortBy is a valid input
+        if(sortBy === 'id' || sortBy === 'type' || sortBy === 'averageRating' || sortBy === 'ratingCount') {
+            return this.jokeRepository.find({
+                order: {
+                    // Fancy use of a variable as a JSON key
+                    [sortBy]: "ASC",
+                },
+            });
+            // If it's not valid, sort by id
+        } else {
+            return this.jokeRepository.find({
+                order: {
+                    id: "ASC",
+                },
+            })
+        }
     }
 
-    @Route('put', '/:id')
+    @Route('put')
     async update(request: Request, response: Response, next: NextFunction) {
-        let jokeToUpdate = await this.jokeRepository.preload(request.body);
-        if(!jokeToUpdate || jokeToUpdate.id != request.params.id) next();
-        else {
-            let violations = await validate(jokeToUpdate, this.validOptions);
-            if (violations.length) {
-                response.statusCode = 422;
-                return violations;
-            } else {
+        let userRating = Object.assign(new UserRating(), request.body);
+        let violations = await validate(userRating, this.validOptions);
+        if(violations.length) {
+            response.statusCode = 422// Unprocessable Entity
+            return violations;
+        } else {
+            let jokeToUpdate = await this.jokeRepository.preload(await this.jokeRepository.findOne(userRating.id));
+            if(!jokeToUpdate) next();
+            else {
+                jokeToUpdate.averageRating = ((jokeToUpdate.averageRating * jokeToUpdate.ratingCount) + userRating.userRating) / (jokeToUpdate.ratingCount + 1);
+                jokeToUpdate.ratingCount++;
                 return this.jokeRepository.save(jokeToUpdate);
             }
         }
@@ -52,11 +86,12 @@ export class JokeController {
                 const violations = await validate(newJoke, this.validOptions);
                 console.log(violations);
                 if (violations.length) {
-                    response.statusCode = 422; // Uncrossable Entity
+                    response.statusCode = 422; // Unprocessable Entity
                     failArray.push(newJoke);
                 } else {
                     response.statusCode = 201; // Created
                     jokeCount++;
+                    console.log("Joke saved");
                     await this.jokeRepository.save(newJoke);
                 }
             }
@@ -69,7 +104,7 @@ export class JokeController {
             const violations = await validate(newJoke, this.validOptions);
             console.log(violations);
             if (violations.length) {
-                response.statusCode = 422; // Uncrossable Entity
+                response.statusCode = 422; // Unprocessable Entity
                 return violations;
             } else {
                 response.statusCode = 201; // Created
@@ -80,10 +115,12 @@ export class JokeController {
 
     @Route('delete', '/:id')
     async remove(request: Request, response: Response, next: NextFunction) {
-        const jokeToRemove = await this.jokeRepository.findOne(request.params.id);
-        response.statusCode = 204; // No Content
-        if (jokeToRemove) return this.jokeRepository.remove(jokeToRemove);
-        else next(); // let index.ts catch the 404 error and reply with JSON
+        if(request.body.id && request.body.id == request.params.id) {
+            response.statusCode = 204;
+            return this.jokeRepository.remove(await this.jokeRepository.findOne(request.params.id));
+        } else {
+            next(createError(422));
+        }
     }
 
 }
